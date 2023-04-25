4661194b78c440806befe154f31910aa8e17c364
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-25 16:13:04 +0300

    Add ability model test

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,18 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+    user.admin? ? admin_abilities(user) : user_abilities(user)
+  end
 
-    return unless user.persisted?
+  private
 
+  def admin_abilities(_user)
+    can :manage, :all
+  end
+
+  def user_abilities(user)
     can :manage, User, id: user.id
     can :manage, Expense, author_id: user.id
+    can :manage, Category, author_id: user.id
   end
 end
