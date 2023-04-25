ec3d0fc9fc11f60e0845e4a93dfe5e00306d7cfd
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-25 12:51:55 +0300

    Add user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
   has_many :expenses, foreign_key: :author_id
-  has_many :categories
+  has_many :categories, through: :expenses
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 end
