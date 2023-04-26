7d8158698e406962da772a0e118589d9bb169606
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-27 01:02:07 +0300

    Update UI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,32 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", () => {
+  // flash messages
+  const flashMessagesContainer = document.querySelector("#flash-messages");
+  if (flashMessagesContainer) {
+    const delay = 5000;
+    setTimeout(() => {
+      console.log("hiding flash messages");
+      flashMessagesContainer.style.display = "none";
+      // flashMessagesContainer.classList.add("hidden");
+    }, delay);
+  }
+
+  const currentUser = JSON.parse(document.querySelector('body').dataset.currentUser);
+
+  if (currentUser) {
+    const hamburgerBtn = document.getElementById("hamburger-btn");
+    const popup = document.getElementById("popup");
+    const closePopupBtn = document.getElementById("close-popup-btn");
+
+    hamburgerBtn.addEventListener("click", () => {
+      popup.classList.remove("hidden");
+    });
+
+    closePopupBtn.addEventListener("click", () => {
+      popup.classList.add("hidden");
+    });
+  }
+});
