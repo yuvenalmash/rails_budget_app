bf6c65cdb25d979f7b40a06b2e4bcb00e55a7c7a
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-28 19:06:16 +0300

    Add helper methods

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,7 +5,29 @@ class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 1, maximum: 20 }
   validates :icon, presence: true
 
+  ICONS = [
+    "fa-solid fa-utensils",
+    "fa-solid fa-tshirt",
+    "fa-solid fa-gas-pump",
+    "fa-solid fa-shopping-cart",
+    "fa-solid fa-credit-card",
+    "fa-solid fa-bus",
+    "fa-solid fa-subway",
+    "fa-solid fa-train",
+    "fa-solid fa-bolt",
+    "fa-solid fa-water",
+    "fa-solid fa-heartbeat",
+    "fa-solid fa-gamepad",
+    "fa-solid fa-question",
+    "fa-solid fa-bowling-ball",
+    "fa-solid fa-band-aid"
+  ]
+
   def total_expenses
     expenses.sum(:amount)
+  end
+
+  def category_expenses(current_user)
+    expenses.where.not(name: "dummy-expense-#{current_user.id}")
   end
 end
